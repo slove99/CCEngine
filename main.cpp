b5751602a0c3d3cf3ca5827e819038f1b94d4aca
unknown <slove99@github.com>
2021-09-11 21:34:52 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Your First C++ Program
+#include <iostream>
+
+// Board object
+class Board {
+    public:
+        int boardState
+}
+
+
+// Piece object - containing player
+// Board object
+class Board {
+    public:
+        int player
+        int x
+        int y
+}
+
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
