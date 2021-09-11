a35dd25612f3a29f2db575d23c5da2579a514e3f
samuel <slove99@github.com>
2021-09-11 22:40:04 +0100

    create board object

--- main.cpp
+++ main.cpp
@@ -1,24 +1,42 @@
 // Your First C++ Program
+
 #include <iostream>
+#include <vector>
 
 // Board object
 class Board {
     public:
-        int boardState
-}
+        int** boardState; // Store game board
+
+    Board(){
+        // Define 17x17 board array
+        boardState = new int*[17]; // Pointer array to 17 int arrays of length 17
+        for (int i=0; i<17; i++)
+            boardState[i] = new int[17]; // Create int arrays
+        }
+
+    void removeBoard(){
+        // Remove 17x17 board array
+        for (int i=0; i<17; i++)
+            delete[] boardState[i]; // Deallocate memory associated with each int array
+        delete[] boardState; // Deallocate pointer array
+    }
+};
 
 
 // Piece object - containing player
-// Board object
-class Board {
+class Piece {
     public:
-        int player
-        int x
-        int y
-}
+        int player;
+        int x;
+        int y;
+};
 
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!\n";
+    Board gameBoard;
+    //gameBoard.boardState = 5;
+    std::cout << gameBoard.boardState << "\n";
     return 0;
 }
